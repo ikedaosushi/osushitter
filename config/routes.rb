5edfee982a9ac4f3f2d39e667cc70882b94db401
池田雄太郎 <ikedayutaro@ikedayutaro-mac.local>
2018-04-16 20:56:21 +0900

    Chapter7

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  resources :users
 
+  get 'users/new'
   root 'static_pages#home'
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
