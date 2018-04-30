c0f44c7be5eb25648abe21507c1d3725147d4f89
Ikeda Yutao <yutaro.ikeda@kaizenplatform.com>
2018-04-30 17:17:19 +0900

    Chapter7

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def gravator_for(user)
     gravator_id = Digest::MD5::hexdigest(user.email.downcase)
-    grabator_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    gravator_url = "https://secure.gravatar.com/avatar/#{gravator_id}"
+    image_tag(gravator_url, alt: user.name, class: "gravatar")
   end
 end
